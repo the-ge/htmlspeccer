418f3d1542d70fc2e6da36016986c80522c1ede8
the-ge <g1704578400@tenita.eu>
2026-08-01 01:39:28 +0300

    refactor(emending): split back emendations single emendation per file

--- .dev/emendations/normalize/11__get_dictified__attributes__append-type__2026-07-26-081231.py
+++ .dev/emendations/normalize/11__get_dictified__attributes__append-type__2026-07-26-081231.py
@@ -1,6 +1,6 @@
 from config import TERSE_DATA_DIR
-from filtering_engine import AriaRoleTerseData, InputTypeTerseData
-from normalizing_engine import AttributeData, parse_aria_roles, parse_input_types
+from filtering_engine import InputTypeTerseData
+from normalizing_engine import AttributeData, parse_input_types
 from util import parse_section
 
 
@@ -8,14 +8,6 @@ def emend(section: str, data: list) -> bool:
     if section != 'attributes':
         return False
 
-    parsed = list(parse_section(TERSE_DATA_DIR, 'aria', 'aria_roles', AriaRoleTerseData, parse_aria_roles))
-    data.append(AttributeData(
-        name='role',
-        description='ARIA semantic role',
-        value_enum={x.name for x in parsed},
-        separator=' ',
-        urls={x.url for x in parsed},
-    ))
     parsed = list(parse_section(TERSE_DATA_DIR, 'input', 'input_types', InputTypeTerseData, parse_input_types))
     data.append(AttributeData(
         name='type',
@@ -25,4 +17,5 @@ def emend(section: str, data: list) -> bool:
         value_info='An input type e.g. "text", "number", or "week".',
         urls={x.url for x in parsed},
     ))
+
     return True
