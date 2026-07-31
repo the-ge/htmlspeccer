4f71f415f5c246ddcc55099de24065248354c997
the-ge <g1704578400@tenita.eu>
2026-08-01 00:56:25 +0300

    refactor(emending): join emendations for normalizing attributes in a single file

--- .dev/emendations/normalize/get_dictified__attributes__2026-07-26-081231.py
+++ .dev/emendations/normalize/get_dictified__attributes__2026-07-26-081231.py
@@ -1,13 +1,21 @@
 from config import TERSE_DATA_DIR
-from filtering_engine import InputTypeTerseData
-from normalizing_engine import AttributeData, parse_input_types
+from filtering_engine import AriaRoleTerseData, InputTypeTerseData
+from normalizing_engine import AttributeData, parse_aria_roles, parse_input_types
 from util import parse_section
 
 
-def emend(section: str, data: list) -> None:
+def emend(section: str, data: list) -> bool:
     if section != 'attributes':
         return False
 
+    parsed = list(parse_section(TERSE_DATA_DIR, 'aria', 'aria_roles', AriaRoleTerseData, parse_aria_roles))
+    data.append(AttributeData(
+        name='role',
+        description='ARIA semantic role',
+        value_enum={x.name for x in parsed},
+        separator=' ',
+        urls={x.url for x in parsed},
+    ))
     parsed = list(parse_section(TERSE_DATA_DIR, 'input', 'input_types', InputTypeTerseData, parse_input_types))
     data.append(AttributeData(
         name='type',
